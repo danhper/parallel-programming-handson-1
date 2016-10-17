ef51c2c5051bfe4f6d6cce27b3d1267e3afd1c47
Daniel Perez <daniel@claudetech.com>
2016-10-17 19:22:02 +0900

    Fix scale.sh to run each try 3 times

--- src/scale.sh
+++ src/scale.sh
@@ -2,5 +2,5 @@
 
 for p in 1 2 4 6 8 12 16 24 32 40 48 56 64 ; do
   echo "Running with $p cores"
-  MYTH_NUM_WORKERS=${p} ./kdtree_main 4194304 1 30
+  MYTH_NUM_WORKERS=${p} ./kdtree_main 4194304 3 30
 done
